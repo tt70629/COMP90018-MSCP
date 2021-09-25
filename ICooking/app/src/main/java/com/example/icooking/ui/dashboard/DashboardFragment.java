717aafc60381cf3ccb4884c3448d22f37182ab49
tt70629 <63644792+tt70629@users.noreply.github.com>
2021-09-25 15:54:48 +1000

    Set up RecyclerView for inventory in dashboard fragment
    1. set up connection between adaptor and helpers.
    2. create pseudo data to mimic data from DB.

--- ICooking/app/src/main/java/com/example/icooking/ui/dashboard/DashboardFragment.java
+++ ICooking/app/src/main/java/com/example/icooking/ui/dashboard/DashboardFragment.java
@@ -7,13 +7,18 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.icooking.R;
+import com.example.icooking.Inventory;
 import com.example.icooking.databinding.FragmentDashboardBinding;
+import com.example.icooking.helper.InventoryItemTouchHelperCallBack;
+
+import java.util.ArrayList;
 
 public class DashboardFragment extends Fragment {
 
@@ -28,13 +33,33 @@ public class DashboardFragment extends Fragment {
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        //pseudo data for testing which should come from database or other source.
+        ArrayList<Inventory> inventory= new ArrayList<Inventory>();
+        inventory.add( new Inventory("milk","1"));
+        inventory.add( new Inventory("ribeye steak","2"));
+        inventory.add( new Inventory("chicken","2"));
+        inventory.add( new Inventory("onion","3"));
+        /*
+         * set up TextView...
+         */
+
         final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        dashboardViewModel.getText().observe(getViewLifecycleOwner(), s -> textView.setText(s));
+        /*
+         * set up RecyclerView...
+         */
+        final RecyclerView recviewInventory= binding.recviewInventory;
+        InventoryAdaptor adaptor= new InventoryAdaptor();
+        adaptor.setInventory(inventory);
+        recviewInventory.setAdapter(adaptor);
+        recviewInventory.setLayoutManager( new LinearLayoutManager(getContext()));
+        recviewInventory.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));//horizontal line between items.
+        /*
+         * use ItemTouchHelper to realize animation effect...
+         */
+        ItemTouchHelper.Callback callback = new InventoryItemTouchHelperCallBack(adaptor);
+        ItemTouchHelper inventoryItemTouchHelper = new ItemTouchHelper(callback);
+        inventoryItemTouchHelper.attachToRecyclerView(recviewInventory);
         return root;
     }
 
