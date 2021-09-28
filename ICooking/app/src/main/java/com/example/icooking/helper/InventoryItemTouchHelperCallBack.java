b8cf02616f76cf901b6e497bdcf6b09354a767a8
tt70629 <63644792+tt70629@users.noreply.github.com>
2021-09-28 22:27:03 +1000

    Remove onChildDraw and temporary not using this class

--- ICooking/app/src/main/java/com/example/icooking/helper/InventoryItemTouchHelperCallBack.java
+++ ICooking/app/src/main/java/com/example/icooking/helper/InventoryItemTouchHelperCallBack.java
@@ -1,19 +1,33 @@
 package com.example.icooking.helper;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.util.Log;
+import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.icooking.R;
 import com.example.icooking.helper.ItemTouchHelperAdaptor;
 
 public class InventoryItemTouchHelperCallBack extends ItemTouchHelper.Callback {
     private final ItemTouchHelperAdaptor adaptorCallBack;
+    private final Context context;
 
-    public InventoryItemTouchHelperCallBack(ItemTouchHelperAdaptor adaptorCallBack){
+    public InventoryItemTouchHelperCallBack(ItemTouchHelperAdaptor adaptorCallBack, Context context){
         this.adaptorCallBack = adaptorCallBack;
+        this.context=context;
 
     }
 
@@ -32,18 +46,50 @@ public class InventoryItemTouchHelperCallBack extends ItemTouchHelper.Callback {
 
     @Override
     public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
-         adaptorCallBack.onItemDismiss(viewHolder.getAdapterPosition());
+        //if (direction==ItemTouchHelper.LEFT){
+            //adaptorCallBack.onItemDismiss(viewHolder.getAdapterPosition());
+        }
+
     }
 
-    @Override
-    public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
-        if(actionState==ItemTouchHelper.ACTION_STATE_SWIPE){
-           swipedItem(dX, viewHolder);
-        }
-        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
-    }
-    private void swipedItem(float dX,RecyclerView.ViewHolder viewHolder){
-        float alpha= 1-Math.abs(dX)/viewHolder.itemView.getWidth();
-        System.out.println("hi there");
-    }
-}
+//    @Override
+//    public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+//        try {
+//            Bitmap icon;
+//            if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+//                View itemView = viewHolder.itemView;
+//                Paint p = new Paint ();
+//                float height = (float) itemView.getBottom() - (float) itemView.getTop();
+//                float width = height / 5;
+//                viewHolder.itemView.setTranslationX(dX / 5);
+//
+//                p.setColor(Color.parseColor("#D32F2F"));
+//                RectF background = new RectF(
+//                        (float) (itemView.getRight() + dX / 5),
+//                        (float) itemView.getTop(),
+//                        (float) itemView.getRight(),
+//                        (float) itemView.getBottom());
+//                c.drawRect(background, p);
+//                icon = BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_delete);
+//
+//                RectF icon_dest = new RectF(
+//                        (float) (itemView.getRight() + dX/5+ 60),
+//                        (float) itemView.getTop()+width,
+//                        (float) (itemView.getRight() + dX/5+ 60)+100,
+//                        (float) itemView.getBottom()-width);
+//
+//                c.drawBitmap(icon, null, icon_dest, p);
+//
+//            } else {
+//                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//
+//    }
+
+
+
+
