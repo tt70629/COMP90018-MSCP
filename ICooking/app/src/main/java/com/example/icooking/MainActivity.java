bf3b29f69bc2dbe51b2a3065255bd15e0dcf4da6
tt70629 <63644792+tt70629@users.noreply.github.com>
2021-10-24 19:48:07 +1100

    Remove attribute of lefday as it was a buggy function
    1. Calculate left day dynamically.
    2. Sort ingredient by day left.
    3. progress bar for disconnection from db

--- ICooking/app/src/main/java/com/example/icooking/MainActivity.java
+++ ICooking/app/src/main/java/com/example/icooking/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.icooking;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -22,16 +27,27 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        final ProgressBar pgsBar=findViewById(R.id.progressBar_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_inventory, R.id.navigation_notifications)
+                 R.id.navigation_inventory,R.id.navigation_home, R.id.navigation_notifications)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+    public static boolean isConnected(Context context){
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifiInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo mobileInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+        if ((wifiInfo != null && wifiInfo.isConnected()) || (mobileInfo != null && mobileInfo.isConnected())) {
+            return true;
+        }else{
+            return false;
+        }
     }
 
 }
